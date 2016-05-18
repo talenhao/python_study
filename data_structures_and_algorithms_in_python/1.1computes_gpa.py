3220f65ef9bf116a4511fd2bb9f1a1248bf15cd4
talenhao <talenhao@gmail.com>
2016-05-18 15:08:20 +0800

    use delimiter '''

--- data_structures_and_algorithms_in_python/1.1computes_gpa.py
+++ data_structures_and_algorithms_in_python/1.1computes_gpa.py
@@ -1,6 +1,9 @@
-print( 'Welcome to the GPA calculator.' )
-print('Please inter all your letter grades, one per line.')
-print('Enter a blank line to designate the end.')
+#print( 'Welcome to the GPA calculator.' )
+#print('Please inter all your letter grades, one per line.')
+#print('Enter a blank line to designate the end.')
+print( '''Welcome to the GPA calculator.
+Please inter all your letter grades, one per line.
+Enter a blank line to designate the end.''')
 #map from letter grade to point value
 points = {'A+':4.0, 'A':4.0, 'A-':3.67, 'B+':3.33, 'B':3.0, 'B-':2.67, 'C+':2.33, 'C':2.0, 'C':1.67, 'D+':1.33, 'D':1.0, 'F':0.0}
 num_courses = 0
