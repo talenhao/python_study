d19b9c526c1bc356204ffd9b405a0f5064c7adb8
talenhao <talenhao@gmail.com>
2016-05-19 17:27:20 +0800

    添加是否启用新API?

--- head_first_python/movies.py
+++ head_first_python/movies.py
@@ -2,5 +2,11 @@
 #-*- coding:utf8 -*-
 import nester
 movies = ['The Holy Grail', 1975, 'Terry Jones & Terry Gilliam', 91, ['Graham Chapman', ['Michael Palin', 'John Cleese', 'Terry Gilliam', 'Eric Idle', 'Terry Jones']]]
-nester.print_lol(movies,1)
+def versions(version):
+    print('version is %s' % version)
+versions('1.1.0')
 nester.print_lol(movies)
+versions('1.1.1')
+nester.print_lol(movies,1)
+versions('1.2.0')
+nester.print_lol(movies, 'Ture', 2)
--- head_first_python/nester.py
+++ head_first_python/nester.py
@@ -3,15 +3,20 @@
 '''
 打印电影信息
 version = 1.1.1
+api evolustion:default value => set anathor bool value
 '''
-def print_lol(the_list, level=0):
+def print_lol(the_list, indent=False, level=0):
     for each_item in the_list:
         '''
         判断元素是否为列表类型.
         '''
         if isinstance(each_item, list) :
-            print_lol(each_item, level+1)
+            print_lol(each_item, indent, level+1)
         else:
-            for tab_stop in range(level):
-                print('\t',end='.')
+            '''
+            是否启用新API
+            '''
+            if indent:
+                for tab_stop in range(level):
+                    print('\t',end='.')
             print(each_item)
