e8b61d2cdac519519a4043dcd8385e7af626e358
talenhao <talenhao@gmail.com>
2016-05-19 16:55:21 +0800

    add level, version 1.1.0

--- head_first_python/movies.py
+++ head_first_python/movies.py
@@ -2,4 +2,4 @@
 #-*- coding:utf8 -*-
 import nester
 movies = ['The Holy Grail', 1975, 'Terry Jones & Terry Gilliam', 91, ['Graham Chapman', ['Michael Palin', 'John Cleese', 'Terry Gilliam', 'Eric Idle', 'Terry Jones']]]
-nester.print_lol(movies)
+nester.print_lol(movies,1)
--- head_first_python/nester.py
+++ head_first_python/nester.py
@@ -2,13 +2,16 @@
 #-*- coding:utf-8 -*-
 '''
 打印电影信息
+version = 1.1.0
 '''
-def print_lol(the_list):
+def print_lol(the_list, level):
     for each_item in the_list:
         '''
         判断元素是否为列表类型.
         '''
         if isinstance(each_item, list) :
-            print_lol(each_item)
+            print_lol(each_item, level+1)
         else:
+            for tab_stop in range(level):
+                print('\t',end='.')
             print(each_item)
