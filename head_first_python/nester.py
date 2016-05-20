d3cdbb5c10e81ece888f114ce5e3703f9bb69bd3
talenhao <talenhao@gmail.com>
2016-05-20 16:22:06 +0800

    use nester.print_lol() format list and write to file

--- head_first_python/nester.py
+++ head_first_python/nester.py
@@ -5,18 +5,20 @@
 version = 1.1.1
 api evolustion:default value => set anathor bool value
 '''
-def print_lol(the_list, indent=False, level=0):
+#20160520:添加输出参数设定
+import sys
+def print_lol(the_list, indent=False, level=0, fh=sys.stdout):
     for each_item in the_list:
         '''
         判断元素是否为列表类型.
         '''
         if isinstance(each_item, list) :
-            print_lol(each_item, indent, level+1)
+            print_lol(each_item, indent, level+1, fh)
         else:
             '''
             是否启用新API
             '''
             if indent:
                 for tab_stop in range(level):
-                    print('\t',end='.')
-            print(each_item)
+                    print('\t',end='.', file=fh)
+            print(each_item, file=fh)
