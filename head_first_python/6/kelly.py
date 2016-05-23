8c2526a7944339ab220b04a4113e171d30c8c050
talenhao <talenhao@gmail.com>
2016-05-23 11:52:29 +0800

    use list

---
+++ head_first_python/6/kelly.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+def filetolist(file,listname):
+    try:
+        #打开文件
+        with open(file) as jaf:
+            #读取数据行
+            data = jaf.readline()
+        #转换成list
+        listname=data.strip().split(',')
+        return listname
+    except IOError as ioerr:
+        print('File error : %s' % ioerr)
+        return(None)
+        
+#处理字符,转换成m.s格式
+def sanitize(time_string):
+    if '-' in time_string:
+        splitter = '-'
+    elif ':' in time_string:
+        splitter = ':'
+    else:
+        return time_string
+    (min, sec) = time_string.split(splitter)
+    return (min + '.' + sec)
+
+for name in ["james", "julie", "mikey", "sarah"]:
+    thelist=filetolist(name+".txt",name)
+    username=name+'user'
+    userdob =name+'dob'
+    username = thelist.pop(0)
+    userdob  = thelist.pop(0)
+    #使用列表推导式
+    name2 = [sanitize(each_it) for each_it in thelist]
+    #使用工厂函数set()
+    try:
+        print(username + '的最佳成绩是' + str(sorted(set(name2))[0:3]))
+#        print(username + '的最佳成绩是' + sorted(set(name2))[0:3])
+    except TypeError as typerr:
+        print('list type error %s' % typerr)
