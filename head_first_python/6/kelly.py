8203e94b533941633f0c0ab50bb4520bfcb5f2c0
talenhao <talenhao@gmail.com>
2016-05-23 12:10:36 +0800

    use dict

--- head_first_python/6/kelly.py
+++ head_first_python/6/kelly.py
@@ -28,13 +28,20 @@ for name in ["james", "julie", "mikey", "sarah"]:
     thelist=filetolist(name+".txt",name)
     username=name+'user'
     userdob =name+'dob'
-    username = thelist.pop(0)
-    userdob  = thelist.pop(0)
-    #使用列表推导式
-    name2 = [sanitize(each_it) for each_it in thelist]
-    #使用工厂函数set()
-    try:
-        print(username + '的最佳成绩是' + str(sorted(set(name2))[0:3]))
-#        print(username + '的最佳成绩是' + sorted(set(name2))[0:3])
-    except TypeError as typerr:
-        print('list type error %s' % typerr)
+    #使用列表
+    #username = thelist.pop(0)
+    #userdob  = thelist.pop(0)
+    ##使用列表推导式
+    #name2 = [sanitize(each_it) for each_it in thelist]
+    ##使用工厂函数set()
+    #try:
+    #    print(username + '的最佳成绩是' + str(sorted(set(name2))[0:3]))
+    #except TypeError as typerr:
+    #    print('list type error %s' % typerr)
+    #使用字典
+    data = {}
+    data['name'] = thelist.pop(0)
+    data['dob']  = thelist.pop(0)
+    data['time'] = thelist
+    print(data['name'] + '的三次最佳成绩是' + str(sorted(set([sanitize(each_it) for each_it in data['time']]))[0:3]))
+
