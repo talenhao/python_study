cb56cb00477780317df03da524f64dca10d29914
talenhao <talenhao@gmail.com>
2016-05-24 10:16:33 +0800

    extend from list class

--- head_first_python/6/kelly_c.py
+++ head_first_python/6/kelly_c.py
@@ -6,8 +6,24 @@ class athlete:
         self.name = athlete_name
         self.dob  = athlete_dob
         self.times= athlete_times
+    #运动员最好的3组成绩
     def top3(self):
         return(sorted(set([sanitize(time) for time in self.times]))[0:3])
+    #为运动员添加一个成绩
+    def add_time(self, time_value):
+        self.times.append(time_value)
+    #为运动员添加一组成绩,使用列表类型.
+    def add_times(self, time_list):
+        self.times.extend(time_list)
+#使用类继承,继承内置list类
+class athletelist(list):
+    def __init__(self, a_name, a_dob=None, a_times=[]):
+        list.__init__([])
+        self.name = a_name
+        self.dob  = a_dob
+        self.extend(a_times)
+    def top3(self):
+        return(sorted(set([sanitize(t) for t in self]))[0:3])
 
 def openfile(filename):
     try:
@@ -39,3 +55,17 @@ def sanitize(time_string):
 for name in ["james", "julie", "mikey", "sarah"]:
     name = openfile(name+'.txt')
     print(name.name + '的三次最佳成绩是' + str(name.top3()))
+
+
+talen = athlete('talen')
+talen.add_time('3.25')
+talen.add_time('3.45')
+talen.add_times(['1.30','2.59'])
+print(str(talen.top3()))
+ken = athletelist('ken')
+#为运动员添加一个成绩
+#由于继承list,不需要自己再定义添加方法,直接使用list的方法
+ken.append('4.25')
+#为运动员添加一组成绩,使用列表类型.
+ken.extend(['4.56','6.20','5.20'])
+print(ken.top3())
