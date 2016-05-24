fef37dacc05a33febc22d8993fb0130120bfb825
talenhao <talenhao@gmail.com>
2016-05-24 15:54:00 +0800

    over M

---
+++ head_first_python/7web/webapp/cgi-bin/athletemodel.py
@@ -0,0 +1,65 @@
+#!/usr/bin/evn python3
+# -*- coding:utf8 -*-
+'''
+1.读取文件	=>	put_to_store	=>	pickle
+2.pickle      	=>	get_from_store	=>	viewer 
+'''
+import pickle
+from kelly_c import athletelist
+#磁盘文件处理
+def openfile(filename):
+    try:
+        #打开文件
+        with open(filename) as athlete_file:
+            #读取数据
+            data = athlete_file.readline()
+            #初步处理数据,去空,以,号分割
+            value_list= data.strip().split(',')
+            #分别取出有格式的三种数据
+            username = value_list.pop(0)
+            userdob  = value_list.pop(0)
+            usertimes= value_list
+            #返回实例对象
+            athlete_instance=athletelist(username,userdob,usertimes)
+            return(athlete_instance)
+    except IOError as ioerr:
+        print('File error %s' % ioerr)
+        return(None)
+
+#内容压制,使用字典数据类型.
+def put_to_store(files_list):
+    #字典生成
+    all_athletes = {}
+    for each_file in files_list:
+        each_athlete = openfile(each_file)
+        all_athletes[each_athlete.name] = each_athlete
+    #pickle数据压制
+    try:
+        with open('athletes.pickle','wb') as athlfile:
+            pickle.dump(all_athletes,athlfile)
+    except IOError as ioerr:
+        print('File error(%s)' % ioerr)
+    return(all_athletes)
+
+def get_from_store():
+    all_athletes = {}
+    #pickle数据解压
+    try:
+        with open('athletes.pickle','rb') as athlfile:
+            all_athletes=pickle.load(athlfile)
+    except IOError as ioerr:
+        print('File error(%s)' % ioerr)
+    return(all_athletes)
+
+files_list = ["../data/james.txt", "../data/julie.txt", "../data/mikey.txt", "../data/sarah.txt"]
+data = put_to_store(files_list)
+print(get_from_store())
+print(dir())
+type(data)
+print('Use put_to_store()')
+for each_athlete in data:
+    print(data[each_athlete].name,data[each_athlete].dob)
+print('Use get_from_store()')
+data_copy = get_from_store()
+for each_athlete in data_copy:
+    print(data_copy[each_athlete].name,data_copy[each_athlete].dob)
---
+++ head_first_python/7web/webapp/cgi-bin/kelly_c.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+class athletelist(list):
+    def __init__(self, a_name, a_dob=None, a_times=[]):
+        list.__init__([])
+        self.name = a_name
+        self.dob  = a_dob
+        self.extend(a_times)
+    def top3(self):
+        return(sorted(set([sanitize(t) for t in self]))[0:3])
+
+#处理字符,转换成m.s格式
+def sanitize(time_string):
+    if '-' in time_string:
+        splitter = '-'
+    elif ':' in time_string:
+        splitter = ':'
+    else:
+        return time_string
+    (min, sec) = time_string.split(splitter)
+    return (min + '.' + sec)
