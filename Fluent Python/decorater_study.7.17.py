5703a3d92ad8200bef3705e188ca8379ac8d5bcd
talenhao <talenhao@gmail.com>
2020-05-21 19:23:14 +0800

    functools

--- Fluent Python/decorater_study.7.17.py
+++ Fluent Python/decorater_study.7.17.py
@@ -1,17 +1,32 @@
 import time
 import functools
 """
-[Thu May 21 talen@tp ~]$ /home/talen/.pyenv/versions/3.7.3/bin/python3.7 "/home/talen/github/tianfei/python_study/Fluent Python/decorater_study.7.17.py"
+没有使用functools复制被装饰函数的属性时的输出,可以看到被装饰函数统一显示成clocked
 **************************************** calling snooze(.123)
-[0.12318444] snooze(0.123) -> None
+[0.12316942] snooze(0.123) -> None
+clocked
 **************************************** calling factorial(6)
-[0.00000310] factorial(1) -> 1
-[0.00009680] factorial(2) -> 2
-[0.00016260] factorial(3) -> 6
-[0.00025153] factorial(4) -> 24
-[0.00031281] factorial(5) -> 120
-[0.00038075] factorial(6) -> 720
+[0.00000167] factorial(1) -> 1
+[0.00005102] factorial(2) -> 2
+[0.00008607] factorial(3) -> 6
+[0.00013471] factorial(4) -> 24
+[0.00016856] factorial(5) -> 120
+[0.00020885] factorial(6) -> 720
 6! = 720
+clocked
+使用functools复制被装饰函数的属性时的输出
+**************************************** calling snooze(.123)
+[0.12319827] snooze(0.123) -> None
+snooze
+**************************************** calling factorial(6)
+[0.00000286] factorial(1) -> 1
+[0.00011683] factorial(2) -> 2
+[0.00017142] factorial(3) -> 6
+[0.00024962] factorial(4) -> 24
+[0.00029564] factorial(5) -> 120
+[0.00035572] factorial(6) -> 720
+6! = 720
+factorial
 """ 
 
 
@@ -50,6 +65,8 @@ def factorial(n):
 if __name__ == "__main__":
     print('*' * 40, "calling snooze(.123)")
     snooze(.123)
+    print(snooze.__name__)
     
     print('*' * 40, "calling factorial(6)")
     print('6! =', factorial(6))
+    print(factorial.__name__)
